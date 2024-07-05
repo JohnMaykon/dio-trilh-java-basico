c210011b666125933c500126f6ce23d3b2da5b70
JohnMaykon <johnmikon@gmail.com>
2024-07-04 23:04:43 -0300

    Desafio DIO

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*Conhecer e importar a classe Scanner
+         *Exibir as mensagens para o nosso usuário
+         *Obter pela scanner os valores digitados no terminal
+         *Exibir a mensagem de conta criada*/
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor Digite o Nome do Cliente: ");
+        String nomeCliente = scanner.nextLine();
+        System.out.println("Por favor Digite o numero da conta: ");
+        int conta = scanner.nextInt();
+        System.out.println("Por favor Digite o numero da Agencia: ");
+        int agencia = scanner.nextInt();
+
+        System.out.println("Digite o saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá, "
+                + nomeCliente
+                + " obrigado por criar uma conta em nosso banco, sua agência é: "
+                + agencia
+                + ",sua conta é: " + conta + " e seu saldo:" + saldo + " já está disponível para saque.");
     }
 }
